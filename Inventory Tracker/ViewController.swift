1fe2150a7eeab73296406994b875ad291db6a23a
Finn Frankis <finn@teachmy.com>
2019-06-25 16:33:54 -0700

    Initial Commit

---
+++ Inventory Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Inventory Tracker
+//
+//  Created by Finn Frankis on 6/25/19.
+//  Copyright © 2019 FinnitoProductions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
