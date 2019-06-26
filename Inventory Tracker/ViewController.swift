47b0aa1cae64cf5173e9e4b5db8f77f0453fc76b
Finn Frankis <finn@teachmy.com>
2019-06-25 20:15:24 -0700

    Create basic image selection functionality from photo library

--- Inventory Tracker/ViewController.swift
+++ Inventory Tracker/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
+import Firebase
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var cameraFeed: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func detectBarcodes() {
+//        let format = VisionBarcodeFormat.all
+//        let barcodeOptions = VisionBarcodeDetectorOptions(formats: format)
+//        var vision = Vision.vision()
+//        let barcodeDetector = vision.barcodeDetector(options: barcodeOptions)
+//        let image = VisionImage(image: cameraFeed)
+    }
 
-
+    @IBAction func openPicker(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            cameraFeed.contentMode = .scaleAspectFit
+            cameraFeed.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
-
