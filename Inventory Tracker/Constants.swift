06766f4f0558e1c4a9f537ea027b57698f8c2ec6
Finn Frankis <finn@teachmy.com>
2019-06-25 23:43:47 -0700

    Complete live barcode scanning

---
+++ Inventory Tracker/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Inventory Tracker
+//
+//  Created by Finn Frankis on 6/25/19.
+//  Copyright © 2019 FinnitoProductions. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+enum Constants {
+    static let videoFeedOutputLabel = "com.google.firebaseml.visiondetector.VideoDataOutputQueue"
+    static let defaultCameraPos = AVCaptureDevice.Position.back
+}
--- Inventory Tracker/ViewController.swift
+++ Inventory Tracker/ViewController.swift
@@ -10,46 +10,140 @@ import UIKit
 import AVFoundation
 import Firebase
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UINavigationControllerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate {
     @IBOutlet weak var cameraFeed: UIImageView!
-    let imagePicker = UIImagePickerController()
+    let captureSession = AVCaptureSession()
+    lazy var vision = Vision.vision()
+    @IBOutlet weak var barCodeRawValueLabel: UILabel!
+    
+    var barcodeDetector : VisionBarcodeDetector?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imagePicker.delegate = self
+        barcodeDetector = vision.barcodeDetector(options: VisionBarcodeDetectorOptions(formats: VisionBarcodeFormat.all))
+        setupCameraOutput()
+        setupCameraInput()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    func detectBarcodes(image: UIImage) {
-        print("detecting new barcode")
-        let barcodeOptions = VisionBarcodeDetectorOptions(formats: VisionBarcodeFormat.all)
-        let barcodeDetector = Vision.vision().barcodeDetector(options: barcodeOptions)
-        let image = VisionImage(image: image)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
-        barcodeDetector.detect(in: image) { barcodes, error in
-            guard error == nil, let barcodes = barcodes, !barcodes.isEmpty else {return}
-            barcodes.forEach { barcode in
-                print("\(barcode.valueType) + \(barcode.rawValue)")
+        startCameraSession()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func setupCameraInput() {
+        captureSession.beginConfiguration()
+        guard let device = self.captureDevice(forPosition: Constants.defaultCameraPos) else {
+            return
+        }
+        do {
+            let input = try AVCaptureDeviceInput(device: device)
+            captureSession.addInput(input)
+            captureSession.commitConfiguration()
+        }
+        catch {
+            print("Failed")
+        }
+    }
+    
+    func setupCameraOutput() {
+        captureSession.beginConfiguration()
+        captureSession.sessionPreset = .medium
+        
+        let output = AVCaptureVideoDataOutput()
+        output.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as String): kCVPixelFormatType_32BGRA]
+        output.setSampleBufferDelegate(self, queue: DispatchQueue(label: Constants.videoFeedOutputLabel))
+        
+        captureSession.addOutput(output)
+        captureSession.commitConfiguration()
+        
+        let imageLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        imageLayer.frame = CGRect(x: 0, y: 0, width: self.cameraFeed.frame.size.width + 100, height: self.cameraFeed.frame.size.height)
+        imageLayer.videoGravity = .resizeAspectFill
+        cameraFeed.layer.addSublayer(imageLayer)
+    }
+    
+    func startCameraSession() {
+        captureSession.startRunning()
+    }
+    
+    func startBarcodeCapture() {
+        captureSession.sessionPreset = .photo
+        let captureDevice = AVCaptureDevice.default(for: .video)
+        
+        let deviceInput = try! AVCaptureDeviceInput(device: captureDevice!)
+        
+        let deviceOutput = AVCaptureVideoDataOutput()
+        deviceOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA)]
+        deviceOutput.setSampleBufferDelegate(self, queue: DispatchQueue.global(qos: DispatchQoS.QoSClass.default))
+        
+        captureSession.addInput(deviceInput)
+        captureSession.addOutput(deviceOutput)
+        
+
+        
+        captureSession.startRunning()
+    }
+    
+    func detectBarcodes(buffer: CMSampleBuffer) {
+        let image = VisionImage(buffer: buffer)
+        
+        let metadata = VisionImageMetadata()
+        metadata.orientation = imageOrientation(deviceOrientation: UIDevice.current.orientation, cameraPosition: .back)
+        image.metadata = metadata
+        
+        if let barcodeDetector = self.barcodeDetector {
+            barcodeDetector.detect(in: image) { barcodes, error in
+                guard error == nil, let barcodes = barcodes else {
+                    if let error = error {
+                        print(error.localizedDescription)
+                    }
+                    return
+                }
+                
+                
+                barcodes.forEach { barcode in
+                    print("found")
+                    self.barCodeRawValueLabel.text = "\(barcode.rawValue)"
+                    self.barCodeRawValueLabel.textColor = .green
+                }
             }
         }
     }
-
-    @IBAction func openPicker(_ sender: Any) {
-        imagePicker.allowsEditing = false
-        imagePicker.sourceType = .camera
-        present(imagePicker, animated: true, completion: nil)
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        detectBarcodes(buffer: sampleBuffer)
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            cameraFeed.contentMode = .scaleAspectFit
-            cameraFeed.image = pickedImage
-            detectBarcodes(image: pickedImage)
+    private func imageOrientation(
+        deviceOrientation: UIDeviceOrientation,
+        cameraPosition: AVCaptureDevice.Position
+        ) -> VisionDetectorImageOrientation {
+        switch deviceOrientation {
+        case .portrait:
+            return cameraPosition == .front ? .leftTop : .rightTop
+        case .landscapeLeft:
+            return cameraPosition == .front ? .bottomLeft : .topLeft
+        case .portraitUpsideDown:
+            return cameraPosition == .front ? .rightBottom : .leftBottom
+        case .landscapeRight:
+            return cameraPosition == .front ? .topRight : .bottomRight
+        case .faceDown, .faceUp, .unknown:
+            return .leftTop
         }
-        dismiss(animated: true, completion: nil)
     }
     
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        dismiss(animated: true, completion: nil)
+    private func captureDevice(forPosition position: AVCaptureDevice.Position) -> AVCaptureDevice? {
+        if #available(iOS 10.0, *) {
+            let discoverySession = AVCaptureDevice.DiscoverySession(
+                deviceTypes: [.builtInWideAngleCamera],
+                mediaType: .video,
+                position: .unspecified
+            )
+            return discoverySession.devices.first { $0.position == position }
+        }
+        return nil
     }
 }
